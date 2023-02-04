530e2e4ec950ca2909c52c3b07cab1705aa981c1
bakhridinova <bakhridinovaleyla@gmail.com>
2023-02-04 13:22:23 +0500

    updated StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,16 +1,11 @@
 package com.epam.utils;
 
-import java.math.BigDecimal;
-
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            if (Double.parseDouble(str) <= 0) {
-                return false;
-            }
+            return Double.parseDouble(str) > 0;
         } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
-        return true;
     }
 }
